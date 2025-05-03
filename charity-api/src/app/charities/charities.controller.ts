9ae505b442658608200387c630c6084da718f6aa
Alan <107621256+alan14112003@users.noreply.github.com>
2025-05-03 16:13:22 +0700

    refactor: add JwtAuthGuard to create, update, and remove methods in Charities and Programs controllers for enhanced security

--- charity-api/src/app/charities/charities.controller.ts
+++ charity-api/src/app/charities/charities.controller.ts
@@ -6,16 +6,19 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { CharitiesService } from './charities.service';
 import { CreateCharityDto } from './dto/create-charity.dto';
 import { UpdateCharityDto } from './dto/update-charity.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('charities')
 export class CharitiesController {
   constructor(private readonly charitiesService: CharitiesService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createCharityDto: CreateCharityDto) {
     return this.charitiesService.create(createCharityDto);
   }
@@ -31,11 +34,13 @@ export class CharitiesController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateCharityDto: UpdateCharityDto) {
     return this.charitiesService.update(+id, updateCharityDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.charitiesService.remove(+id);
   }
--- charity-api/src/app/programs/programs.controller.ts
+++ charity-api/src/app/programs/programs.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { ProgramsService } from './programs.service';
 import { CreateProgramDto } from './dto/create-program.dto';
 import { UpdateProgramDto } from './dto/update-program.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('programs')
 export class ProgramsController {
   constructor(private readonly programsService: ProgramsService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createProgramDto: CreateProgramDto) {
     return this.programsService.create(createProgramDto);
   }
@@ -23,11 +34,13 @@ export class ProgramsController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateProgramDto: UpdateProgramDto) {
     return this.programsService.update(+id, updateProgramDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.programsService.remove(+id);
   }
